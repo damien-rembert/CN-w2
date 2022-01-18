2325adb72d34c25e68951c094ab8a887cc87e8c4
Damien R <damien.rembert@gmail.com>
2022-01-18 16:46:13 +0000

    things happened

---
+++ day1.js
@@ -0,0 +1,9 @@
+// Declare an array that stores a shopping list.
+// This array should have three categories (arrays) of the
+// shopping items and each of these categories should be
+// an array with at least three shopping items.
+// Log the 3rd item of the 2nd list of items to the console.
+
+let shopping = [["potato", "tomato", "leek"] , ["coke", "wine", "juice"], ["flour", "sugar"] ];
+console.log(shopping[1][2]);
+
